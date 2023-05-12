1a959c3fd8a3b9f573570ef2792a1a17d46392c5
Agustin Legizamon <agustin.leg7@gmail.com>
2023-05-11 22:55:06 -0300

    refactor: :lipstick: add google colors

--- src/components/Blur.tsx
+++ src/components/Blur.tsx
@@ -12,13 +12,13 @@ export default function Blur(props: IconProps) {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <circle cx="71" cy="61" r="111" fill="#F56565" />
-      <circle cx="244" cy="106" r="139" fill="#ED64A6" />
-      <circle cy="291" r="139" fill="#ED64A6" />
-      <circle cx="80.5" cy="189.5" r="101.5" fill="#ED8936" />
-      <circle cx="196.5" cy="317.5" r="101.5" fill="#ECC94B" />
-      <circle cx="70.5" cy="458.5" r="101.5" fill="#48BB78" />
-      <circle cx="426.5" cy="-0.5" r="101.5" fill="#4299E1" />
+      <circle cx="71" cy="61" r="111" fill="#4285F4" />
+      <circle cx="244" cy="106" r="139" fill="#E94235" />
+      <circle cy="291" r="139" fill="#FABB06" />
+      <circle cx="80.5" cy="189.5" r="101.5" fill="#4285F4" />
+      <circle cx="196.5" cy="317.5" r="101.5" fill="#34A854" />
+      <circle cx="70.5" cy="458.5" r="101.5" fill="#FABB06" />
+      <circle cx="426.5" cy="-0.5" r="101.5" fill="#34A854" />
     </Icon>
   );
 }
